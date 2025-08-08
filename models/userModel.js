6c1ce2884bbee76a6a806d6e3d6d67b3db3d454c
uaroundserver <ruslanlotnik@gmail.com>
2025-08-08 07:43:19 +0200

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,12 @@
 const { ObjectId } = require('mongodb');
 
 class User {
-    constructor({ email, password, isActive = false, phone = null }) {
+    constructor({ email, password, isActive = false, phone = null, country = null }) {
         this.email = email;
         this.password = password; // В реальном проекте пароль нужно хешировать!
         this.isActive = isActive; // подтверждён ли email
         this.phone = phone;       // номер телефона
+        this.country = country;   // страна проживания
         this._id = new ObjectId(); // уникальный ID
     }
 }
